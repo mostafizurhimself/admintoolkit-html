e8042e1a981d07aad65851db8b0dd8907fe9c5ce
mostafizurhimself <mostafizurhimself@gmail.com>
2022-12-31 20:14:26 +0600

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,60 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+// Add your custom theme colors here
+const themeColors = {
+  primary: colors.violet,
+  secondary: colors.gray,
+  success: colors.emerald,
+  warning: colors.amber,
+  danger: colors.rose,
+  info: colors.indigo,
+  dark: colors.slate,
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,css,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      transitionProperty: {
+        width: 'width',
+        height: 'height',
+        margin: 'margin',
+      },
+      keyframes: {
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 250ms ease-in-out',
+      },
+      colors: themeColors,
+      variables: {
+        DEFAULT: themeColors,
+      },
+      borderRadius: {
+        primary: '0.4rem',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@mertasan/tailwindcss-variables'),
+    require('tailwindcss-rtl'),
+    require('@tailwindcss/typography'),
+  ],
 }
