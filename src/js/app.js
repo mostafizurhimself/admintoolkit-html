1786f82653bc07dea9098b9db3b9f28c6bff56e4
mostafizurhimself <mostafizurhimself@gmail.com>
2023-01-03 21:35:10 +0600

    Add dropdown component

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,17 @@
-import highlightCode from "./highlight";
-
+import highlightCode from './highlight';
 
 highlightCode();
+
+const initDropdown = () => {
+  const dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns.forEach((dropdown) => {
+    const toggle = dropdown.querySelector('.dropdown-toggle');
+    const content = dropdown.querySelector('.dropdown-content');
+
+    toggle.addEventListener('click', () => {
+      content.classList.toggle('show');
+    });
+  });
+};
+
+initDropdown();
