3cfc34e7dd35e1c0a674f3441cd48c681406f863
mostafizurhimself <mostafizurhimself@gmail.com>
2023-01-03 22:07:38 +0600

    Add seperation dropdown javascript file

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,5 @@
+import dropdown from './dropdown';
 import highlightCode from './highlight';
 
 highlightCode();
-
-const initDropdown = () => {
-  const dropdowns = document.querySelectorAll('.dropdown');
-  dropdowns.forEach((dropdown) => {
-    const toggle = dropdown.querySelector('.dropdown-toggle');
-    const content = dropdown.querySelector('.dropdown-content');
-
-    toggle.addEventListener('click', () => {
-      content.classList.toggle('show');
-    });
-  });
-};
-
-initDropdown();
+dropdown.init();
