f70a161c0d4a473ea039e13f7b153e324f805aa6
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-01-07 18:40:11 +0600

    [Feat] Enable dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,34 @@ import accordion from './accordion';
 import highlightCode from './highlight';
 import feather from 'feather-icons';
 
+
+const switchThemeMode  = () => {
+
+     const btnThemeMode = document.querySelector('#btn-theme-mode');
+
+     btnThemeMode.addEventListener('click', () => {
+
+          if(document.documentElement.classList.contains('dark')) {
+               //switch light mode
+               localStorage.theme = 'light';
+               document.documentElement.classList.remove('dark');
+               
+          }else {
+
+               //switch dark mode
+               localStorage.theme = 'dark';
+               document.documentElement.classList.add('dark');
+
+          }
+     });
+}
+
 highlightCode.init();
 dropdown.init();
 accordion.init();
+
+//Initialize theme mode
+switchThemeMode();
 
 // Initialize feather icons
 feather.replace();
