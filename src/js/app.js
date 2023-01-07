6a94a5fd726e2d9e29d7599abf51e3f047a691ed
mostafizurhimself <mostafizurhimself@gmail.com>
2023-01-07 14:36:43 +0600

    [Feat] add feather icon

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
 import dropdown from './dropdown';
 import accordion from './accordion';
 import highlightCode from './highlight';
+import feather from 'feather-icons';
 
 highlightCode.init();
 dropdown.init();
 accordion.init();
+
+// Initialize feather icons
+feather.replace();
