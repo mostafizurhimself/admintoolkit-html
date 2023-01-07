039ebf3c66a8089c8eaebf58bd6bfa2682564e01
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-01-07 22:38:44 +0600

    write theme code in theme.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ import theme from './theme';
 import highlightCode from './highlight';
 import feather from 'feather-icons';
 
-
 highlightCode.init();
 dropdown.init();
 accordion.init();
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,25 +1,38 @@
 const theme = {
-     
-     init() {
-          
-          const btnThemeMode = document.querySelector('#btn-theme-mode');
 
-          btnThemeMode.addEventListener('click', () => {
+     themeModeBtn: document.querySelector('#btn-theme-mode'),
+
+     init() {
+
+          const themeMode = localStorage.getItem('theme-mode');
+          const themeModeBtn = this.themeModeBtn;
+
+          if (themeMode) {
+
+               document.documentElement.classList.add(themeMode);
+          } 
+
+          themeModeBtn.addEventListener('click', this.toggle);
+          
+     },
+
+     toggle() {
+
+          if(document.documentElement.classList.contains('dark')) {
+               
+               //switch to light mode 
+               localStorage.setItem('theme-mode', 'light');
+               document.documentElement.classList.remove('dark');
+
+          }else {
+
+               //switch to dark mode
+               localStorage.setItem('theme-mode', 'dark');
+               document.documentElement.classList.add('dark');
+          }
      
-               if(document.documentElement.classList.contains('dark')) {
-                    //switch light mode
-                    localStorage.theme = 'light';
-                    document.documentElement.classList.remove('dark');
-                    
-               }else {
-     
-                    //switch dark mode
-                    localStorage.theme = 'dark';
-                    document.documentElement.classList.add('dark');
-     
-               }
-          });
-     }
+     }    
+
 }
 
 export default theme;
