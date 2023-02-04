7bd885b2faf05510680f513eafe79a879a90f99b
mostafizurhimself <mostafizurhimself@gmail.com>
2023-02-04 15:35:32 +0600

    refactor(dropdown): add floating ui to position dropdown

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -1,21 +1,73 @@
+import { computePosition, offset, flip, shift } from '@floating-ui/dom';
+
+class Dropdown {
+  constructor(target) {
+    if (typeof target === 'string') {
+      this.target = document.querySelector(target);
+    }
+
+    if (target instanceof HTMLElement) {
+      this.target = target;
+    }
+
+    if (!this.target) {
+      throw new Error('No target element found');
+    }
+
+    this.toggle = this.target.querySelector('.dropdown-toggle');
+    this.content = this.target.querySelector('.dropdown-content');
+
+    if (!this.toggle) {
+      throw new Error('No toggle element found');
+    }
+
+    if (!this.content) {
+      throw new Error('No content element found');
+    }
+
+    this.init();
+  }
+
+  init() {
+    this.renderContent();
+  }
+
+  renderContent() {
+    computePosition(this.toggle, this.content, {
+      placement: 'bottom-end',
+      strategy: 'fixed',
+      middleware: [flip(), shift(), offset(6)],
+    }).then((position) => {
+      Object.assign(this.content.style, {
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      });
+    });
+  }
+
+  registerEvents() {}
+}
+
 const dropdown = {
   init() {
     const dropdowns = document.querySelectorAll('.dropdown');
     dropdowns.forEach((dropdown) => {
-      const toggle = dropdown.querySelector('.dropdown-toggle');
-      const content = dropdown.querySelector('.dropdown-content');
+      new Dropdown(dropdown);
 
-      toggle.addEventListener('click', () => {
-        content.classList.toggle('show');
-        content.classList.toggle('animate-fade-in-up');
-      });
+      // const toggle = dropdown.querySelector('.dropdown-toggle');
+      // const content = dropdown.querySelector('.dropdown-content');
 
-      document.addEventListener('click', (e) => {
-        if (!dropdown.contains(e.target)) {
-          content.classList.remove('show');
-          content.classList.remove('animate-fade-in-up');
-        }
-      });
+      // toggle.addEventListener('click', () => {
+      //   content.classList.toggle('show');
+      //   content.classList.toggle('animate-fade-in-up');
+      // });
+
+      // document.addEventListener('click', (e) => {
+      //   if (!dropdown.contains(e.target)) {
+      //     content.classList.remove('show');
+      //     content.classList.remove('animate-fade-in-up');
+      //   }
+      // });
     });
   },
 };
