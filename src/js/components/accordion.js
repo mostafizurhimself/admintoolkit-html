e17efddaa738ebe7d9db8b2eca79d579a1e19d4a
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-02-05 18:09:47 +0600

    Feat: add accordion component

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -1,41 +1,83 @@
+class Accordion {
+
+  constructor(accordion) {
+    this.accordionItems   = accordion.querySelectorAll('.accordion-item');
+
+    [...this.accordionItems].forEach((accordionItem) => {
+        const accordionBtn = accordionItem.querySelector('.accordion-btn');
+
+        accordionBtn.addEventListener('click', () => {
+          [...this.accordionItems].forEach((accordionItem) => {
+            if(accordionItem.classList.contains('active')) {
+              this.hide(accordionItem);
+            }
+          });
+
+          this.toggle(accordionItem);
+        })
+    });
+
+  }
+
+  toggle(accordionItem) {
+
+    if(!accordionItem.classList.contains('active')) {
+
+      this.show(accordionItem);
+
+    }else {
+
+      this.hide(accordionItem);
+    }
+  }
+
+  show(accordionItem) {
+    const accordionContent = accordionItem.querySelector('.accordion-content');
+
+    accordionItem.classList.add('active');
+    accordionContent.style.height = '0px';
+
+    setTimeout(() => {
+
+      accordionContent.style.height = accordionContent.scrollHeight + 'px';
+
+    }, 5);
+
+    setTimeout(() => {
+
+      accordionContent.removeAttribute('style');
+
+    }, 300);
+  }
+
+  hide(accordionItem) {
+    const accordionContent = accordionItem.querySelector('.accordion-content');
+
+    accordionContent.style.height = accordionContent.scrollHeight + 'px';
+
+    setTimeout(() => {
+
+      accordionContent.style.height = '0px';
+
+    }, 5)
+
+    setTimeout(() => {
+      
+      accordionItem.classList.remove('active');
+      accordionContent.removeAttribute('style');
+
+    }, 300)
+  }
+}
+
 const accordion = {
   init() {
     const accordions = document.querySelectorAll('.accordion');
 
-    accordions.forEach((accordion) => {
-      const accordionBtn = accordion.querySelector('.accordion-btn');
-
-      accordionBtn.addEventListener('click', () => {
-        const accordionContent = accordion.querySelector('.accordion-content');
-
-        if (accordionContent) {
-          if (!accordion.classList.contains('open')) {
-            //open the accordion
-            accordion.classList.add('open');
-
-            accordionContent.style.height = 0;
-
-            setTimeout(() => {
-              accordionContent.style.height = accordionContent.scrollHeight + 'px';
-            }, 10);
-          } else {
-            //close the accordion
-            accordionContent.style.height = accordionContent.scrollHeight + 'px';
-
-            setTimeout(() => {
-              accordionContent.style.height = 0;
-            }, 10);
-
-            setTimeout(() => {
-              accordion.classList.remove('open');
-
-              accordionContent.removeAttribute('style');
-            }, 10);
-          }
-        }
-      });
-    });
-  },
-};
+    if(accordions.length) {
+      [...accordions].forEach((accordion) => new Accordion(accordion));
+    }
+  }
+}
 
 export default accordion;
