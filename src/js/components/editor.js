5369ec7ccfe4723c92fdb95d4d92aa75656f270c
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-02-18 14:04:40 +0600

    refactor: update buble text editor

--- src/js/components/editor.js
+++ src/js/components/editor.js
@@ -43,13 +43,12 @@ const editor  = {
             toolbar:[
               ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
               ['blockquote', 'code-block'],
-              [{ 'align': [] }],
+            
               [{ 'header': 1 }, { 'header': 2 }],               // custom button values
               [{ 'list': 'ordered'}, { 'list': 'bullet' }],
               [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-              [{ 'indent': '-1'}, { 'indent': '+1' }],
-              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-              [{ 'font': [] }],
+              [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+              [{ 'direction': 'rtl' }], 
             ]
           },
           theme: 'bubble'
