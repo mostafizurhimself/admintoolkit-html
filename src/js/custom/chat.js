85ef74349794e05f1763e04656b25f183d98e62f
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-02-27 18:19:55 +0600

    fix: prettier code into chat page

--- src/js/custom/chat.js
+++ src/js/custom/chat.js
@@ -1,9 +1,9 @@
 // Default Scroll To Bottom
-window.addEventListener('load',()=>{
-    const chats = document.getElementById('chats');
-    chatScrollHeight = chats.scrollHeight;
-    chats.scrollTo({top: chatScrollHeight, left:0});
-})
+window.addEventListener('load', () => {
+  const chats = document.getElementById('chats');
+  chatScrollHeight = chats.scrollHeight;
+  chats.scrollTo({ top: chatScrollHeight, left: 0 });
+});
 //  Chat List Toggle
 const chatList = document.getElementById('chat-list');
 const hideChatList = document.getElementById('hide-chat-list');
@@ -11,27 +11,27 @@ const openChatList = document.getElementById('open-chat-list');
 const overly = document.getElementById('overly');
 // Open Chat List
 const getOpenChatList = () => {
-    chatList.classList.remove("-translate-x-full");
-    chatList.classList.add("translate-x-0");
-    overly.classList.remove('invisible','opacity-0')
-    overly.classList.add('visible','opacity-100')
-}
+  chatList.classList.remove('-translate-x-full');
+  chatList.classList.add('translate-x-0');
+  overly.classList.remove('invisible', 'opacity-0');
+  overly.classList.add('visible', 'opacity-100');
+};
 
 // Hide Chat List
 const getHideChatList = () => {
-    overly.classList.remove('opacity-100')
-    overly.classList.add('opacity-0')
-    overly.classList.remove('visible')
-    setTimeout(() => {
-    chatList.classList.remove("translate-x-0");
-    chatList.classList.add("-translate-x-full");
-    overly.classList.add('invisible')
-    },100);
-} 
+  overly.classList.remove('opacity-100');
+  overly.classList.add('opacity-0');
+  overly.classList.remove('visible');
+  setTimeout(() => {
+    chatList.classList.remove('translate-x-0');
+    chatList.classList.add('-translate-x-full');
+    overly.classList.add('invisible');
+  }, 100);
+};
 
-//  EventListeners 
-openChatList.addEventListener('click', getOpenChatList) 
-hideChatList.addEventListener('click', getHideChatList)
-chatList.addEventListener('click',(event)=>{
-    if(event.target === chatList) getHideChatList();
-})
+//  EventListeners
+openChatList.addEventListener('click', getOpenChatList);
+hideChatList.addEventListener('click', getHideChatList);
+chatList.addEventListener('click', (event) => {
+  if (event.target === chatList) getHideChatList();
+});
