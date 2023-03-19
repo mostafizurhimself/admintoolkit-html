1a8b784615178dec18f0844eff3fdff4abd26a48
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-03-19 20:57:57 +0600

    fix: hide controlled alert

--- src/js/custom/alert.js
+++ src/js/custom/alert.js
@@ -1,29 +1,29 @@
-const controlledAlertSource = `
+const controlledAlertSourceCode = `
   <div class="flex flex-col gap-4">
     <div class="alert alert-success" role="alert" id="controlled-alert">
-      <span class="alert-icon"><i width="1em" height="1em" data-feather="check-circle"></i></span>
-      <p class="alert-text">This is a success alert with addtional information and content.</p>
-    </div>
+      <i width="1em" height="1em" data-feather="check-circle"></i>
+      <p>This is a success alert with addtional information and content.</p>
+    </div> 
     <div>
-      <button class="btn btn-primary" type="button" onclick="dismissControlledAlert()">
+      <button class="btn btn-primary" type="button" id="btn-hide-alert">
         Hide dismissable alert
       </button>
     </div>
   </div>
 
   <script>
-    const controlledAlert = createDismissableAlert('#controlled-alert');
+    const btnHideAlert = document.querySelector('#btn-hide-alert');
 
-    function dismissControlledAlert() {
-      controlledAlert.dismiss();
-    }
+    btnHideAlert.addEventListener('click', () => {
+      createDismissableAlert('#controlled-alert').dismiss();
+    })
   </script>
 `;
 
-const controlledCodeViewer = createCodeViewer('#controlled-code-viewer', controlledAlertSource);
+const controlledAlertCodeViewer = createCodeViewer('#controlled-alert-code-viewer', controlledAlertSourceCode);
+controlledAlertCodeViewer.render();
 
-function dismissControlledAlert() {
+const btnHideAlert = document.querySelector('#btn-hide-alert');
+btnHideAlert.addEventListener('click', () => {
   createDismissableAlert('#controlled-alert').dismiss();
-}
-
-controlledCodeViewer.render();
+})
