1fc142cd6a038d1a46172d596e1a570fe7c60147
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-03-26 16:59:26 +0600

    build: add column chart into apex chart compo

--- src/js/components/apex-charts.js
+++ src/js/components/apex-charts.js
@@ -336,12 +336,78 @@ const apexCharts = {
             }
           }
           },
+        columnChart:{
+          series: [{
+          name: 'PRODUCT A',
+          data: [44, 55, 41, 67, 22, 43]
+        }, {
+          name: 'PRODUCT B',
+          data: [13, 23, 20, 8, 13, 27]
+        }, {
+          name: 'PRODUCT C',
+          data: [11, 17, 15, 15, 21, 14]
+        }, {
+          name: 'PRODUCT D',
+          data: [21, 7, 25, 13, 22, 8]
+        }],
+          chart: {
+          type: 'bar',
+          height: 350,
+          stacked: true,
+          toolbar: {
+            show: false
+          },
+          zoom: {
+            enabled: true
+          }
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: 'bottom',
+              offsetX: -10,
+              offsetY: 0
+            }
+          }
+        }],
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            borderRadius: 10,
+            dataLabels: {
+              total: {
+                enabled: true,
+                style: {
+                  fontSize: '13px',
+                  color:'#94A3B8',
+                  fontWeight: 900
+                }
+              }
+            }
+          },
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+            '01/05/2011 GMT', '01/06/2011 GMT'
+          ],
+        },
+        legend: {
+          position: 'right',
+          offsetY: 40
+        },
+        fill: {
+          opacity: 1
+        }
+        }
     },
 
 
     init() {
         const lineCharts = document.querySelectorAll('.line-chart');
-        const areaCharts = document.querySelectorAll('.area-chart')
+        const areaCharts = document.querySelectorAll('.area-chart');
+        const columnCharts = document.querySelectorAll('.column-chart');
         // Line Charts 
         if(lineCharts.length){
             [...lineCharts].forEach(lineChart => {
@@ -353,6 +419,13 @@ const apexCharts = {
         if(areaCharts.length){
             [...areaCharts].forEach(areaChart => {
                 const chart = new ApexCharts(areaChart, this.options.areaChart);
+                chart.render();
+            })
+        }
+        // column Charts 
+        if(columnCharts.length){
+            [...columnCharts].forEach(columnChart => {
+                const chart = new ApexCharts(columnChart, this.options.columnChart);
                 chart.render();
             })
         }
