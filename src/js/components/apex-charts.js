cfada9153547667ae6186f02e6bedf872226ab90
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-03-26 17:26:21 +0600

    build: add bar chart into apex chart compo

--- src/js/components/apex-charts.js
+++ src/js/components/apex-charts.js
@@ -400,7 +400,130 @@ const apexCharts = {
         fill: {
           opacity: 1
         }
+        },
+        barChart:{
+          series: [
+          {
+            name: 'Actual',
+            data: [
+              {
+                x: '2011',
+                y: 12,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 14,
+                    strokeWidth: 2,
+                    strokeDashArray: 2,
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2012',
+                y: 44,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 54,
+                    strokeWidth: 5,
+                    strokeHeight: 10,
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2013',
+                y: 54,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 52,
+                    strokeWidth: 10,
+                    strokeHeight: 0,
+                    strokeLineCap: 'round',
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2014',
+                y: 66,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 61,
+                    strokeWidth: 10,
+                    strokeHeight: 0,
+                    strokeLineCap: 'round',
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2015',
+                y: 81,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 66,
+                    strokeWidth: 10,
+                    strokeHeight: 0,
+                    strokeLineCap: 'round',
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2016',
+                y: 67,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 70,
+                    strokeWidth: 5,
+                    strokeHeight: 10,
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+          chart: {
+          height: 350,
+          type: 'bar',
+          toolbar:{
+            show: false
+          }
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+          }
+        },
+        colors: ['#00E396'],
+        dataLabels: {
+          formatter: function(val, opt) {
+            const goals =
+              opt.w.config.series[opt.seriesIndex].data[opt.dataPointIndex]
+                .goals
+        
+            if (goals && goals.length) {
+              return `${val} / ${goals[0].value}`
+            }
+            return val
+          }
+        },
+        legend: {
+          show: true,
+          showForSingleSeries: true,
+          customLegendItems: ['Actual', 'Expected'],
+          markers: {
+            fillColors: ['#00E396', '#775DD0']
+          }
         }
+        },
     },
 
 
@@ -408,6 +531,7 @@ const apexCharts = {
         const lineCharts = document.querySelectorAll('.line-chart');
         const areaCharts = document.querySelectorAll('.area-chart');
         const columnCharts = document.querySelectorAll('.column-chart');
+        const barCharts = document.querySelectorAll('.bar-chart');
         // Line Charts 
         if(lineCharts.length){
             [...lineCharts].forEach(lineChart => {
@@ -426,6 +550,13 @@ const apexCharts = {
         if(columnCharts.length){
             [...columnCharts].forEach(columnChart => {
                 const chart = new ApexCharts(columnChart, this.options.columnChart);
+                chart.render();
+            })
+        }
+        // bar Charts 
+        if(barCharts.length){
+            [...barCharts].forEach(barChart => {
+                const chart = new ApexCharts(barChart, this.options.barChart);
                 chart.render();
             })
         }
