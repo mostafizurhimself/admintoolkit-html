c10d3b0d9fb8b63a6e0837fc197773113f112230
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-03-26 17:04:07 +0600

    feat: add form validation

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,7 @@ import carousel from './components/carousel';
 import editor from './components/editor';
 import select from './components/select';
 import uploader from './components/uploader';
+import validation from './components/validation';
 
 // Initialize themeSwitcher
 themeSwitcher.init();
@@ -55,6 +56,9 @@ select.init();
 
 // Initialize uploader
 uploader.init();
+
+// Initialize validation
+validation.init();
 
 // Initialize datepicker
 datepicker.init();
