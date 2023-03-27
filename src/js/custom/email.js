24c08f4bc9aea119a0cae06b74abd79763c2d568
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-03-27 20:07:32 +0600

    refactor: update email list width into email page

--- src/js/custom/email.js
+++ src/js/custom/email.js
@@ -1,31 +1,32 @@
 //  Chat List Toggle
-const emailList = document.getElementById('email-list');
-const hideEmailList = document.getElementById('hide-email-list');
-const openEmailList = document.getElementById('open-email-list');
-const overlay = document.getElementById('overlay');
+const emailList = document.getElementById("email-list");
+const hideEmailList = document.getElementById("hide-email-list");
+const openEmailList = document.getElementById("open-email-list");
+const overlay = document.getElementById("overlay");
 // Open Email List
 const getOpenEmailList = () => {
-  emailList.classList.remove('-translate-x-full');
-  emailList.classList.add('translate-x-0');
-  overlay.classList.remove('invisible', 'opacity-0');
-  overlay.classList.add('visible', 'opacity-100');
+  emailList.classList.remove("-translate-x-full");
+  emailList.classList.add("translate-x-0");
+  overlay.classList.remove("invisible", "opacity-0");
+  overlay.classList.add("visible", "opacity-100");
 };
 
 // Hide Email List
 const getHideEmailList = () => {
-  overlay.classList.remove('opacity-100');
-  overlay.classList.add('opacity-0');
-  overlay.classList.remove('visible');
+  overlay.classList.remove("opacity-100");
+  overlay.classList.add("opacity-0");
+  overlay.classList.remove("visible");
   setTimeout(() => {
-    emailList.classList.remove('translate-x-0');
-    emailList.classList.add('-translate-x-full');
-    overlay.classList.add('invisible');
+    emailList.classList.remove("translate-x-0");
+    emailList.classList.add("-translate-x-full");
+    overlay.classList.add("invisible");
   }, 100);
 };
 
 //  EventListeners
-openEmailList.addEventListener('click', getOpenEmailList);
-hideEmailList.addEventListener('click', getHideEmailList);
-emailList.addEventListener('click', (event) => {
+openEmailList.addEventListener("click", getOpenEmailList);
+overlay.addEventListener("click", getHideEmailList);
+hideEmailList.addEventListener("click", getHideEmailList);
+emailList.addEventListener("click", (event) => {
   if (event.target === emailList) getHideEmailList();
 });
