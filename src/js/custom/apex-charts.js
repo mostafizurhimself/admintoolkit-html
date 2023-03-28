54049359ddad961d21af07c7890dceff63bffd31
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-03-28 16:01:50 +0600

    refactor: update bar chart into apex charts

--- src/js/custom/apex-charts.js
+++ src/js/custom/apex-charts.js
@@ -254,6 +254,43 @@ const chartOptions = {
       },
     },
   },
+  barChart: {
+    series: [
+      {
+        data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350,
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: [
+        "South Korea",
+        "Canada",
+        "United Kingdom",
+        "Netherlands",
+        "Italy",
+        "France",
+        "Japan",
+        "United States",
+        "China",
+        "Germany",
+      ],
+    },
+  },
 };
 // Line Chart Start
 const lineChartElement = document.querySelector("#line-chart");
@@ -333,3 +370,15 @@ createCodeViewer(
 ).render();
 
 // column Chart End
+
+// bar Chart Start
+const barChartElement = document.querySelector("#bar-chart");
+const barChart = new ApexCharts(barChartElement, chartOptions.barChart);
+barChart.render();
+
+createCodeViewer(
+  "#bar-chart-code-viewer",
+  sourceCodeTemplate("bar-chart")
+).render();
+
+// bar Chart End
