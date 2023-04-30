5d310e4d208dff8f487e3c762412a5c5af63d01c
mostafizurhimself <mostafizurhimself@gmail.com>
2023-04-30 12:49:30 +0600

    feat: reset search modal state

--- src/js/components/search-modal.js
+++ src/js/components/search-modal.js
@@ -3,7 +3,34 @@ import searchData from '@/json/search-data.json';
 import recentSearchData from '@/json/recent-search-data.json';
 
 export class SearchModal {
-  constructor(target) {
+
+  constructor(trigger, target) {    
+    this.setTrigger(trigger);
+    this.setTarget(target);
+    this.setModal();
+    this.setSearchInput();
+
+    this.registerEvents();
+    this.renderSearchContent(recentSearchData);
+  }
+
+  // Set the trigger element
+  setTrigger(trigger) { 
+    if (typeof trigger === 'string') {
+      this.trigger = document.querySelector(trigger);
+    }
+
+    if (trigger instanceof HTMLElement) {
+      this.trigger = trigger;
+    }
+
+    if (!this.trigger) {
+      throw new Error('No trigger element found');
+    }
+  }
+
+  // Set the target element
+  setTarget(target) { 
     if (typeof target === 'string') {
       this.target = document.querySelector(target);
     }
@@ -15,20 +42,48 @@ export class SearchModal {
     if (!this.target) {
       throw new Error('No target element found');
     }
+  }
 
-    this.registerEvents();
-    this.renderSearchContent(recentSearchData);
+  // Set the modal
+  setModal() { 
+    this.modal = createModal(this.target);
+  }
+
+  // Set the search input 
+  setSearchInput() {  
+    const searchInput = this.target.querySelector('input[type="text"]');
+    if(!searchInput) {
+      throw new Error('No search input found');
+    }
+    this.searchInput = searchInput;
+  }
+
+  // Show the modal
+  show() { 
+    this.modal.show();
+  }
+
+  // Hide the modal
+  hide() { 
+    this.modal.hide();
   }
 
   // Register events
   registerEvents() {
-    const searchInput = this.target.querySelector('input[type="text"]');
-    searchInput.addEventListener('input', (e) => this.handleInput(e));
+
+    // Handle trigger click
+    this.trigger.addEventListener('click', () => {
+      this.searchInput.value = '';
+      this.show();
+      this.renderSearchContent(recentSearchData);
+    });
+
+    // Handle input change
+    this.searchInput.addEventListener('input', (e) => this.handleInput(e.target.value));
   }
 
   // Handle input change
-  handleInput(e) {
-    const search = e.target.value;
+  handleInput(search) {
 
     if (search.length === 0) {
       this.renderSearchContent(recentSearchData);
@@ -110,10 +165,12 @@ export class SearchModal {
 
 const searchModal = {
   init() {
+    const triggers = document.querySelectorAll('[data-trigger="search-modal"]');
     const target = document.getElementById('search-modal');
-    if (target) {
-      new SearchModal(target);
-    }
+
+    triggers.forEach((trigger) => { 
+      new SearchModal(trigger, target);
+    });
   },
 };
 
